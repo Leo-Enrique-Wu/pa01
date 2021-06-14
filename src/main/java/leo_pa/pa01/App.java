9e9ffece319723175b211f1731c5d20fe29e210a
Chia-Hao Wu <hedgehogboy@gmail.com>
2021-06-14 15:31:37 -0400

    Initial commit

---
+++ src/main/java/leo_pa/pa01/App.java
@@ -0,0 +1,13 @@
+package leo_pa.pa01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
